52286c38f815e237dfd5cb57e7a966d442e469b0
Eric Christensen <hebrew.8.12@gmail.com>
2021-11-16 17:00:06 -0800

    init commit

---
+++ src/routes/Admin.tsx
@@ -0,0 +1,11 @@
+import cx from "./Admin.module.scss";
+
+const Admin = () => {
+  return (
+    <>
+      <h2 className={cx["test"]}>Admin</h2>
+    </>
+  );
+};
+
+export default Admin;
---
+++ src/routes/Home.tsx
@@ -0,0 +1,12 @@
+import Header from "../containers/Home/Header";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <h2>Home</h2>
+    </>
+  );
+};
+
+export default Home;
